32131cc1923e443c8543288d042566b37e409385
Balyuk Oleg <olegverstka@yandex.ru>
2015-03-08 22:24:38 +0300

    Переделка стартовой каталога

--- wp-content/plugins/goods-catalog/templates/archive-goods.php
+++ wp-content/plugins/goods-catalog/templates/archive-goods.php
@@ -29,25 +29,39 @@ $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
 	</div>
     <hr />
     <div class="catalog-wrap">
+
 <?php
- // categiries list with images
 $category_id = get_query_var('cat');
 $args = array(
-                'type' => 'goods',
-                'taxonomy' => 'goods_category',
-                'hide_empty' => 1,
-                'parent' => 0,
-                'orderby' => 'ID',
-                'child_of' => 0
+                'type'                     => 'goods',
+                'taxonomy'                 => 'goods_category',
+                'hide_empty'               => 1,
+                'parent'                   => 0,
+                'orderby'                  => 'ID',
+                'child_of'                 => 0,
+                'hierarchical'             => 1,
             );
-$category_list = get_categories($args, $category_id);
-//load_template(dirname(__FILE__) . '/loop-grid.php');
+$cats = get_categories($args, $category_id); // Получаем список всех категорий
+    foreach ($cats as $cat) {
+        echo "<h3>".$cat->cat_name."</h3>"; // Получаем название одной категории
+        echo "<ul class='uldotted'>";
+            $post_goods = new WP_Query( array('goods_category' => $cat->category_nicename) ); ?>
+            <?php if($post_goods->have_posts()) : ?>
+                <div class="aaa">
+                    <?php if($post_goods->have_posts()) : while ($post_goods->have_posts()) : $post_goods->the_post(); ?>
+                        <a href="#"><?php the_title(); ?></a><br />
+                    <?php endwhile; ?>
+                    <?php endif; ?>
+                </div>
+            <?php endif;
+            wp_reset_postdata();
+        echo "</ul>";
+    }
 include 'content-goods_category.php';
 
 echo '</div>'; // catalog-inner
 echo '</div>'; // goods-catalog
 
-//echo '<div class="clear"></div>'; // fix for some themes
 load_template ( dirname( __FILE__ ) . '/sidebar-goods.php' ) ;
 echo '</div>'; // middle
 echo '</div>';
